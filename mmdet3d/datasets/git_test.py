d4f1bab453bac8de77c8acab5d8ef1128b58a047
Changxu Zhang <z993065737@gmail.com>
2023-08-24 12:05:48 +0200

    add git_test.py

---
+++ mmdet3d/datasets/git_test.py
@@ -0,0 +1,8 @@
+from .nuscenes_dataset import NuScenesDataset
+
+class CustomNuScenesDataSet(NuScenesDataset):
+    def __init__(self):
+        super.__init__()
+        
+    def whatever(self):
+        pass
